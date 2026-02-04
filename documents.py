1981b3e357c47148ce94cfcdf11681e81fd257a2
nguyennb28 <nguyennb.coding@gmail.com>
2026-02-04 16:38:32 +0700

    hmmm, fixing use vietnamese

--- documents.py
+++ documents.py
@@ -1,8 +1,8 @@
 import spacy
 from spacy_layout import spaCyLayout
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("xx_sent_ud_sm")
 
 layout = spaCyLayout(nlp)
-doc = layout('./docs/english.pdf')
+doc = layout('./docs/Tieng-Viet-123-Student-book-Demo.pdf')
 print(doc.text)
